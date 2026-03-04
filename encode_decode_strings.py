df8316409a98a0f092dd0d0e8dd781d45f68bda1
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-03-04 18:51:05 -0500

    init

---
+++ encode_decode_strings.py
@@ -0,0 +1,15 @@
+# https://neetcode.io/problems/string-encode-and-decode/question?list=neetcode150
+class Solution:
+
+    def encode(self, strs: list[str]) -> str:
+        pass
+    
+    def decode(self, s: str) -> list[str]:
+        pass
+
+def main():
+    s = Solution()
+    print(s.encode())
+
+if __name__ == '__main__':
+    main()

0f9a17b151c4fb8ba62644167b816eac6354afa9
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-03-06 19:29:04 -0500

    init idea

--- longest_substring.py
+++ longest_substring.py
@@ -1,13 +1,10 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # Greedy
-
-        # thru each character in s
-        # 2 pointers, one for start of substr one lookahead
-        # add each c in substr to set, if c exists then we are done and save if new max
-        
-        # We cannot continue from lookahead pointer as we risk skipping the start ofa longer substring
+        # graph
+        # each letter is a node
+        # to find longest substring we want to search the graph, invert djikstra algo?
+        # Could literally run inverse djikstra on every char in the graph and its faster than naive
         abcxyaz
         tabcdefghijklmnopqrstuvwxyz1a23456789
         

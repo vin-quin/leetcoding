a10a17ceb6d266f89683e6222732063690086002
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-03-08 23:45:38 -0400

    27/30

--- longest_substring.py
+++ longest_substring.py
@@ -1,22 +1,9 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # graph
-        # each letter is a node
-        # to find longest substring we want to search the graph, invert djikstra algo?
-        # Could literally run inverse djikstra on every char in the graph and its faster than naive
-        # abcxyaz
-        # tabcdefghijklmwnopqrstuvwxyz1a2345q6789
-        # For each character in str
-        # Build all substring in a hashmap we go so for abcabcbb
-        # {
-        #   a: abc,
-        #   b: bc,
-        #   c: c,
-        # }
-        # When seeing an existing character in the str, we end the current substr for that letter, save, and start again
-        # This means we're iterating of n_s entries per char at most, up to ~100 at worst so O(N*M)?
-
+        if len(s) == 0:
+            return 0
+        
         freqs = {} # Map of freqs for each substr, when a freq for a substr > 1 it needs to be reset
         longest = 0
         for c in s:
@@ -43,10 +30,9 @@ class Solution:
                         longest = chrLength
 
                     print(f'Resetting substr for {c}')
-                    keyRemovals.append(k)
+                    freqs[k].clear()
+                    freqs[k][c] = 1
             
-            for k in keyRemovals:
-                freqs.pop(k)
             print(f'{freqs=}')
 
         for k in freqs: # Final pass required for end of string with unterminated substrings
@@ -63,7 +49,8 @@ def main():
     s = Solution()
     # print(s.lengthOfLongestSubstring("pwwkew"))
     # print(s.lengthOfLongestSubstring("abcabcbb"))
-    print(s.lengthOfLongestSubstring("bbbb"))
+    # print(s.lengthOfLongestSubstring("bbbb"))
+    print(s.lengthOfLongestSubstring("aab"))
 
 if __name__ == '__main__':
     main()

bdeb83c7e85069d26e345adc11106f634b574b80
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-03-22 19:27:57 -0400

    init

---
+++ combination_sum.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/combination-sum/description/
+class Solution:
+    def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
+        
+
+def main():
+    s = Solution()
+    print(s.combinationSum([2,3,6,7], 7))
+
+if __name__ == '__main__':
+    main()

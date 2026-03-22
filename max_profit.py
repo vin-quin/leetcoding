a1e09938be2e2a19d7cc3f36705b63efd1d21a21
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-03-22 19:13:09 -0400

    init

---
+++ max_profit.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        
+        return 0
+        
+def main():
+    s = Solution()
+    print(s.maxProfit([7,1,5,3,6,4]))
+
+if __name__ == '__main__':
+    main()

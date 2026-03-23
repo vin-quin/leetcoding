79ae1223c0ea0a4e6a24625fcd785d90263ad1f2
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-03-22 20:02:29 -0400

    done

--- combination_sum.py
+++ combination_sum.py
@@ -1,7 +1,30 @@
 # https://leetcode.com/problems/combination-sum/description/
 class Solution:
     def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
-        
+        # Any candidate > target can be skipped
+        candidates.sort()
+        print(candidates)
+
+        perms = []
+        rec(candidates, [], target, perms)
+
+        return [p for p in perms if p is not None]
+
+
+def rec(candidates: list[int], perm:list[int], t: int, perms=[]):
+    if t < 0:
+        return None # No valid perm
+    
+    if t == 0:
+        return perm
+    
+    # perms = []
+
+    for i in range(len(candidates)):
+        perms.append(rec(candidates[i:], [*perm, candidates[i]], t-candidates[i], perms))
+
+    # return perms
+
 
 def main():
     s = Solution()

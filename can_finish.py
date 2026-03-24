042c35798cb415ff4017dbaa0d86a46c0565ea0a
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-03-23 21:56:38 -0400

    init

---
+++ can_finish.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/course-schedule/description/
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
+        
+def main():
+    s = Solution()
+    print(s.canFinish(2, [[1, 0]]))
+    # print(s.canFinish(2, [[1, 0], [0,1]]))
+
+if __name__ == '__main__':
+    main()

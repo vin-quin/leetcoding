f97137452d5487e622117f6c9ea93751964c8cbd
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-03-26 14:26:30 -0400

    close

--- house_robber_2.py
+++ house_robber_2.py
@@ -4,36 +4,58 @@ class Solution:
         if len(nums) <= 3: # Too short to be interesting
             return max(nums) 
         
+        r1, r2 = 0, 0
         lastRobbedIdx = 0
         value = nums[0]
         numRobbed = 1
         maxRobbed = len(nums) // 2 # Circular
-        print(f'{maxRobbed=}')
+        # print(f'{maxRobbed=}')
         
         for i in range(1, len(nums)): # Rollover to first entry again to check properly
-            print(f'{i=}/{lastRobbedIdx=}/{value=}/{numRobbed=}')
+            # print(f'{i=}/{lastRobbedIdx=}/{value=}/{numRobbed=}')
             if (i - lastRobbedIdx) == 1 and nums[lastRobbedIdx] < nums[i] : # Adjacent 
                 value -= nums[lastRobbedIdx]
                 value += nums[i] # rob this instead
-                print(f'Robbing {nums[i]} @ {i} instead of {nums[lastRobbedIdx]} @ {lastRobbedIdx}')
+                # print(f'Robbing {nums[i]} @ {i} instead of {nums[lastRobbedIdx]} @ {lastRobbedIdx}')
                 lastRobbedIdx = i
             elif numRobbed < maxRobbed and (i - lastRobbedIdx) > 1: # Rob it cause we can
                 value += nums[i]
-                print(f'Robbing {nums[i]} @ {i}')
+                # print(f'Robbing {nums[i]} @ {i}')
                 lastRobbedIdx = i
                 numRobbed += 1
 
-        return value
+        r1 = value
+        lastRobbedIdx = 0
+        value = nums[0]
+        numRobbed = 1
+        maxRobbed = len(nums) // 2 # Circular
+        # print(f'{maxRobbed=}')
+        
+        for i in range(0, len(nums)-1): # Rollover to first entry again to check properly
+            # print(f'{i=}/{lastRobbedIdx=}/{value=}/{numRobbed=}')
+            if (i - lastRobbedIdx) == 1 and nums[lastRobbedIdx] < nums[i] : # Adjacent 
+                value -= nums[lastRobbedIdx]
+                value += nums[i] # rob this instead
+                # print(f'Robbing {nums[i]} @ {i} instead of {nums[lastRobbedIdx]} @ {lastRobbedIdx}')
+                lastRobbedIdx = i
+            elif numRobbed < maxRobbed and (i - lastRobbedIdx) > 1: # Rob it cause we can
+                value += nums[i]
+                # print(f'Robbing {nums[i]} @ {i}')
+                lastRobbedIdx = i
+                numRobbed += 1
+
+        r2 = value
+        return max(r1, r2)
 
 
 def main():
     s = Solution()
-    # print(s.rob([2,3,2]))
+    print(s.rob([2,3,2]))
     print(s.rob([1,2,3,1]))
-    # print(s.rob([1,2,3]))
-    # print(s.rob([1,3,1,3,100]))
-    # print(s.rob([200,3,140,20,10]))
-    # print(s.rob([1,2,3,1,1,2,3,1]))
+    print(s.rob([1,2,3]))
+    print(s.rob([1,3,1,3,100]))
+    print(s.rob([200,3,140,20,10]))
+    print(s.rob([1,2,3,1,1,2,3,1]))
 
 if __name__ == '__main__':
     main()

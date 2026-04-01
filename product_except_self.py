f4759e8016d3144d23757bac40afe5c54fd30a4d
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-01 17:18:03 -0400

    init

---
+++ product_except_self.py
@@ -0,0 +1,12 @@
+# https://neetcode.io/problems/products-of-array-discluding-self/question?list=blind75
+class Solution:
+    def solve(self, nums: list[int]) -> List[int]:
+        
+
+
+def main():
+    s = Solution()
+    print(s.solve([1,2,4,6]))
+
+if __name__ == '__main__':
+    main()

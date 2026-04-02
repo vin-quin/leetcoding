c06b6405602915277ce9a84a392c03f93f10884f
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-01 20:19:49 -0400

    done

--- is_valid_sudoku.py
+++ is_valid_sudoku.py
@@ -6,7 +6,6 @@ class Solution:
         Each digit in a column can only appear once (1-9)
         Each digit in 3x3 subgrid can only appear once (1-9)
         '''
-        freqs = {str(i+1): 0 for i in range(9)}
         for i in range(len(board)):
             seenRow = set()
             seenCol = set()
@@ -15,47 +14,53 @@ class Solution:
                     if board[i][j] in seenRow: # Duplicate num in row
                         return False
                     seenRow.add(board[i][j])
-                    freqs[board[i][j]] += 1
 
                 if board[j][i] != '.':
                     if board[j][i] in seenCol: # Duplcate num in col
                         return False
                     seenCol.add(board[j][i])
-                    freqs[board[j][i]] += 1
                 
-        # SUBGRID_N=3
-        # rOffset, cOffset = 0, 0
-        # for i in range(SUBGRID_N):
-        #     for row in range(rOffset, SUBGRID_N+rOffset): 
-        #         for col in range(cOffset, SUBGRID_N+cOffset):
-        for v in freqs.values(): # Pretty sure any non-even frequencies mean the board is invalid
-            if v % 2 != 0:
-                return False
-                
+        SUBGRID_N=3
+        for i in range(SUBGRID_N):
+            subgrids = {0: set(), 1: set(), 2: set()}
+
+            for row in range(i*SUBGRID_N, (i+1)*SUBGRID_N): 
+                for col in range(len(board)):
+                    if board[row][col] == '.':
+                        continue
+
+                    k = col // SUBGRID_N
+
+                    if board[row][col] in subgrids[k]: # Duplicate in 3x3
+                        return False 
+                    
+                    subgrids[k].add(board[row][col])
+
         return True
 
 
 def main():
     s = Solution()
-    print(s.solve([["5","3",".",".","7",".",".",".","."]
-,["6",".",".","1","9","5",".",".","."]
-,[".","9","8",".",".",".",".","6","."]
-,["8",".",".",".","6",".",".",".","3"]
-,["4",".",".","8",".","3",".",".","1"]
-,["7",".",".",".","2",".",".",".","6"]
-,[".","6",".",".",".",".","2","8","."]
-,[".",".",".","4","1","9",".",".","5"]
-,[".",".",".",".","8",".",".","7","9"]]))
+#     print(s.solve([["5","3",".",".","7",".",".",".","."]
+# ,["6",".",".","1","9","5",".",".","."]
+# ,[".","9","8",".",".",".",".","6","."]
+# ,["8",".",".",".","6",".",".",".","3"]
+# ,["4",".",".","8",".","3",".",".","1"]
+# ,["7",".",".",".","2",".",".",".","6"]
+# ,[".","6",".",".",".",".","2","8","."]
+# ,[".",".",".","4","1","9",".",".","5"]
+# ,[".",".",".",".","8",".",".","7","9"]]))
     
-    print(s.solve([["8","3",".",".","7",".",".",".","."]
-,["6",".",".","1","9","5",".",".","."]
-,[".","9","8",".",".",".",".","6","."]
-,["8",".",".",".","6",".",".",".","3"]
-,["4",".",".","8",".","3",".",".","1"]
-,["7",".",".",".","2",".",".",".","6"]
-,[".","6",".",".",".",".","2","8","."]
-,[".",".",".","4","1","9",".",".","5"]
-,[".",".",".",".","8",".",".","7","9"]]))
+    print(s.solve([
+     [".","8","7","6","5","4","3","2","1"]
+    ,["2",".",".",".",".",".",".",".","."]
+    ,["3",".",".",".",".",".",".",".","."]
+    ,["4",".",".",".",".",".",".",".","."]
+    ,["5",".",".",".",".",".",".",".","."]
+    ,["6",".",".",".",".",".",".",".","."]
+    ,["7",".",".",".",".",".",".",".","."]
+    ,["8",".",".",".",".",".",".",".","."]
+    ,["9",".",".",".",".",".",".",".","."]]))
 
 if __name__ == '__main__':
     main()

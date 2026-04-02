8f7d9c7a926dcdf10f5f5d71e175613c18569791
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-01 20:20:30 -0400

    init

---
+++ longest_consecutive.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/longest-consecutive-sequence/description/
+class Solution:
+    def solve(self, nums: list[int]) -> int:
+        
+
+
+def main():
+    s = Solution()
+    print(s.solve([100,4,200,1,3,2]))
+
+if __name__ == '__main__':
+    main()

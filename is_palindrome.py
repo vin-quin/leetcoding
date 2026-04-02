08d91541df6a64763e8ceb058758b85e5edd46a4
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-02 11:18:58 -0400

    init

---
+++ is_palindrome.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/valid-palindrome/
+class Solution:
+    def solve(self, s: str) -> bool:
+        s = s.lower()
+
+
+def main():
+    s = Solution()
+    print(s.solve("A man, a plan, a canal: Panama"))
+
+if __name__ == '__main__':
+    main()

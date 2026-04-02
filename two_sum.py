336200cfd8f64c3c88e17f08ec12d9dffd7057eb
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-02 11:46:31 -0400

    done

--- two_sum.py
+++ two_sum.py
@@ -1,15 +1,24 @@
 # https://leetcode.com/problems/two-sum/
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        for i in  range(0, len(nums)):
+        # one indexed result
+        tried = set()
+        for i in range(len(nums)):
+            s = nums[i]
+            if s in tried: # We won't succeed repeating this work
+                continue
             for j in range(i+1, len(nums)):
-                if nums[i]+nums[j] == target:
-                    return [i, j]
+                if s + nums[j] == target:
+                    return [i+1, j+1]
+                elif s + nums[j] > target:
+                    break
+            tried.add(nums[i])
+        return []
 
-        return [-1, -1]
 
 def main():
     s = Solution()
+    print(s.twoSum([2,7,11,15], 9))
     print(s.twoSum([3,2,4], 6))
 
 if __name__ == '__main__':

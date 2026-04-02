95fbd0875e3074dfa44ee62ee0593a695f8e72d4
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-02 11:55:37 -0400

    init

---
+++ three_sum.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/3sum/description/
+class Solution:
+    def solve(self, nums: list[int]) -> list[list[int]]:
+        ...
+
+
+def main():
+    s = Solution()
+    print(s.solve([-1,0,1,2,-1,-4]))
+
+if __name__ == '__main__':
+    main()

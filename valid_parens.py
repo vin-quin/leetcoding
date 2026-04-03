a85545e77bc33c4b495a0a7bbdef7d6b8a8f6b1d
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-03 17:28:46 -0400

    done

--- valid_parens.py
+++ valid_parens.py
@@ -1,7 +1,32 @@
 # https://leetcode.com/problems/valid-parentheses/description/
 class Solution:
     def solve(self, s: str) -> bool:
-        ...
+        if len(s) <= 1:
+            return False
+        
+        from collections import deque
+        MAPPING = {
+            "{": "}",
+            "[": "]",
+            "(": ")",
+            }
+        
+        stack = deque()
+
+        # push all opening onto stack
+        # when a closing appears, its opening should be at top of stack if it is valid
+        # else invalid
+        for c in s:
+            if c in MAPPING.keys():
+                stack.appendleft(c)
+            elif len(stack) > 0:
+                opener = stack.popleft()
+                if c != MAPPING[opener]:
+                    return False
+            else: # Stack should not be empty
+                return False 
+
+        return len(stack) == 0
 
 
 def main():
@@ -10,6 +35,7 @@ def main():
     print(s.solve("()[]{}"))
     print(s.solve("(]"))
     print(s.solve("([)]"))
+    print(s.solve("(("))
 
 if __name__ == '__main__':
     main()

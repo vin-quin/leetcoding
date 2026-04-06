2059592db2e2f278464e1027c7071323141918be
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-06 18:40:06 -0400

    init

---
+++ daily_temperatures.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/daily-temperatures/description/
+class Solution:
+    def solve(self, temperatures: list[int]) -> list[int]:
+        ...
+
+
+def main():
+    s = Solution()
+    print(s.solve([73,74,75,71,69,72,76,73]))
+
+if __name__ == '__main__':
+    main()

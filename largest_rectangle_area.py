7bcb6636b4d1e6c06dec78d517b36a4680744215
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-07 19:45:30 -0400

    init

---
+++ largest_rectangle_area.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/largest-rectangle-in-histogram/description/
+class Solution:
+    def solve(self, heights: list[int]) -> int:
+        ...
+
+
+def main():
+    s = Solution()
+    print(s.solve([2,1,5,6,2,3]))
+
+if __name__ == '__main__':
+    main()

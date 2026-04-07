0b22f464c4c2c001001bf03e48ec4221e4510869
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-07 17:45:44 -0400

    start

---
+++ car_fleet.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/car-fleet/description/
+class Solution:
+    def solve(self, target: int, position: List[int], speed: List[int]) -> int:
+        return -1
+
+
+def main():
+    s = Solution()
+    print(s.solve(12, [10,8,0,5,3], [2,4,1,1,3]))
+
+if __name__ == '__main__':
+    main()

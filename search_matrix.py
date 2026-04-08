0db5ac1bd9722af5266664b31e36d88920bad95f
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-08 01:31:01 -0400

    done

--- search_matrix.py
+++ search_matrix.py
@@ -2,8 +2,8 @@
 class Solution:
     def solve(self, matrix: list[list[int]], target: int) -> bool:
         # Binary search each row and then once a row is determined binary search that row
-        row = searchRow(matrix, target, 0, len(matrix))
-        r = binarySearch(row, target, 0, len(row))
+        row = searchRow(matrix, target, 0, len(matrix)-1)
+        r = binarySearch(row, target, 0, len(row)-1)
         print(row)
         print(r)
         return  r != -1 
@@ -39,6 +39,8 @@ def main():
     s = Solution()
     print(s.solve([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
     print(s.solve([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 8))
+    print(s.solve([[1]], 2))
+    print(s.solve([[1]], 1))
 
 if __name__ == '__main__':
     main()

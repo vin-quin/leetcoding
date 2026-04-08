181641f2835ad09bb0811c2835c20626d3f1c6bb
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-07 22:17:27 -0400

    temp

--- largest_rectangle_area.py
+++ largest_rectangle_area.py
@@ -4,24 +4,41 @@ class Solution:
         # Take l, r pointer and crunch in, storing max area along the way 
 
         # area = a*b
-        maxArea = 0
-        for i in range(len(heights)):
-            l, r = i, i-1
-            while r < len(heights):
-                area = min(heights[l], heights[r]) * (r - l + 1) # Min gets us the max height to make a rect
-                if area > maxArea:
-                    print(l, r, area)
-                    maxArea = area
-                r += 1
+        realArea = 0
 
-        # O(N^2)
-        return maxArea 
+        theoryArea = {} # Most area possible this idx can have if it went from i to end
+        for i in range(len(heights)):
+            theoryArea[i] = heights[i] * (len(heights) - i)
+
+        print(theoryArea)
+
+        areas = list(theoryArea.items())
+        areas.sort(key=lambda x: x[1], reverse=True)
+        print(areas)
+
+        for v in areas:
+            idx, maxA = v
+            print(idx, maxA)
+
+            if maxA <= realArea: # We dont care to check anymore it cant get better
+                print('donezo')
+                break
+
+            for i in range(idx, len(heights)):
+                currArea = min(heights[idx], heights[i]) * (i-min(idx,1))
+                print(f'{idx=}/{i=}/{currArea=}')
+                if currArea > realArea:
+                    realArea = currArea
+
+        return realArea 
 
 
 def main():
     s = Solution()
     print(s.solve([2,1,5,6,2,3]))
-    print(s.solve([2,4]))
+    # print(s.solve([2,4]))
+    # print(s.solve([7,1,7,2,2,4]))
+    # print(s.solve([1,3,7]))
 
 if __name__ == '__main__':
     main()

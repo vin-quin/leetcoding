56f2ebb0c1ac1fde891f3e796732667977bdb0e3
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-08 17:14:27 -0400

    pause

--- min_eating_speed.py
+++ min_eating_speed.py
@@ -2,20 +2,24 @@
 class Solution:
     def solve(self, piles: list[int], h: int) -> int:
         # Can only eat from one pile at a time
-        [24,18,11,0]
-        [0,3,9,16]
+        piles.sort()
 
-        remaining = [0] * len(piles)
-        for i in range(len(piles)-2, -1, -1):
-            remaining[i] = remaining[i+1] + piles[i+1]
+        if len(piles) == h: # Gotta eat as fast as possible
+            return piles[-1]
+        
+        # Start with median as eatSpeed
+        # Every hour we remove eatSpeed from a pile
+        # If we have eaten all piles before time is up, save this as bestEatSpeed
+        #   Then halve it and repeat
+        # If we ran out of time before finishing all piles
+        #   Add half length to eatSpeed then repeat
+        # If we have eaten everything and used all time, then we are done
+        
+        # Remove speed from a spot every hour we will get an answer O(N)
+        for hour in h: 
 
-        print(remaining)
-        speed = piles[0]
-        print(speed*(h-1))
-        while (speed * (h-1)) < remaining[0]:
-            speed += 1
 
-        return speed
+        return 0
 
 
 def main():

1e84f4ffd9bd9151ce9d781bf878132465e52ff5
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-08 01:15:38 -0400

    init

---
+++ search_matrix.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/search-a-2d-matrix/description/
+class Solution:
+    def solve(self, matrix: list[list[int]], target: int) -> bool:
+        ...
+
+
+def main():
+    s = Solution()
+    print(s.solve([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
+
+if __name__ == '__main__':
+    main()

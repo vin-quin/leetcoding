865e70c7f088343953658cdca456b0360f4fb774
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-07 22:18:16 -0400

    init

---
+++ binary_search.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/binary-search/description/
+class Solution:
+    def solve(self, nums: list[int], target: int) -> int:
+        ...
+
+
+def main():
+    s = Solution()
+    print(s.solve([-1,0,3,5,9,12], 9))
+
+if __name__ == '__main__':
+    main()

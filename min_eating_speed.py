3fd120c038856f1512bc5e9141204e14c9a52a78
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-08 13:31:21 -0400

    init

---
+++ min_eating_speed.py
@@ -0,0 +1,12 @@
+# https://neetcode.io/problems/eating-bananas/question?list=neetcode150
+class Solution:
+    def solve(self, piles: list[int], h: int) -> int:
+        ...
+
+
+def main():
+    s = Solution()
+    print(s.solve([3,6,7,11], 8))
+
+if __name__ == '__main__':
+    main()

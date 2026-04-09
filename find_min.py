c34f5c5762ce018288028c308bf5e0ea8f4f94f5
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-08 21:24:08 -0400

    init

---
+++ find_min.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+class Solution:
+    def solve(self, nums: list[int]) -> int:
+        
+        
+        return -1
+
+
+def main():
+    s = Solution()
+    print(s.solve([3,4,5,1,2]))
+
+if __name__ == '__main__':
+    main()

1c9371a4f3ecc46a1e2c3b9c3050d344019ba723
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-09 21:03:45 -0400

    init

---
+++ reorder_list.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/reorder-list/description/
+class Solution:
+    def solve(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        return None
+
+
+def main():
+    s = Solution()
+    print(s.solve())
+
+if __name__ == '__main__':
+    main()

03570a50dd542a0bf4eb726920010f2656e5d3a4
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-17 16:02:07 -0400

    done

--- character_replacement.py
+++ character_replacement.py
@@ -3,48 +3,50 @@ class Solution:
     def solve(self, s: str, k: int) -> int:
         from collections import defaultdict
 
-        # freq = defaultdict(int)
-        # for c in s:
-        #     freq[c] += 1
+        freq = defaultdict(int)
+        for c in s:
+            freq[c] += 1
 
-        # print(freq)
-        # common = max(freq.items(), key=lambda x: x[1])
-        # print(common)
-
-        # longest = ""
-        # l, r = 0, 1 # Expand window until we bazinga
-        # while r < len(s):
-        #     if (r-l)-common[1] <= k:
-        #         r +=1
-        #     else:
-        #         l +=1
-
-
+        # Replace each char in string with the most freq occurring char while we can.
+        # Kinda like a worm, keep inching forwards til you find an unreplaceable
+        # Then inch your tail back up until you can replace again. Repeat to EOF
         l, r = 0, 0
+        replaceChar = max(freq)
+        replacementsLeft = k
+        maxLen = 0
+
         while r < len(s):
-            window = s[l:r+1]
-            # print(window)
+            if s[r] == replaceChar: # Char already correct keep searching
+                if maxLen < (r-l)+1:
+                    maxLen = (r-l)+1
 
-            freq = defaultdict(int)
-            for c in window:
-                freq[c] += 1
-            # print(freq)
+                r += 1
 
-            if len(window)-max(freq.items(), key=lambda x: x[1])[1] <= k:
-                r +=1
-            else:
-                l +=1
+                continue
 
-        return len(window)
+            # Char needs to be replaced
+            if replacementsLeft > 0:
+                replacementsLeft -= 1
 
+                if maxLen < (r-l)+1:
+                    maxLen = (r-l)+1
 
+                r += 1 # Char has been replaced
+
+            else: # Cant replace anymore tail has to shrink 
+                while s[l] == replaceChar: # Inch up until we found a char we replaced, go past it to recover 1 replacement
+                    l += 1
+                l += 1 # So it continues AFTER the replacement
+                replacementsLeft += 1
+
+        return maxLen
 
 def main():
     s = Solution()
-    print(s.solve("ABAB", 2))
-    print(s.solve("ABABBA", 2))
-    print(s.solve("AABABBA", 1))
-    print(s.solve("AABABBABB", 3))
+    print(s.solve("ABAB", 2), 4)
+    print(s.solve("ABABBA", 2), 5)
+    print(s.solve("AABABBA", 1), 4)
+    print(s.solve("AABABBABB", 3), 8)
 
 if __name__ == '__main__':
     main()

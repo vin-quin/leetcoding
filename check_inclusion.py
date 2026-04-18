1c79b42ddf5f21976aa3526953f526725b79b9c2
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-17 20:24:18 -0400

    init

---
+++ check_inclusion.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/permutation-in-string/description/
+class Solution:
+    def solve(self, s1: str, s2: str) -> bool:
+        ...
+
+
+def main():
+    s = Solution()
+    print(s.solve("ab", "eidbaooo"))
+
+if __name__ == '__main__':
+    main()

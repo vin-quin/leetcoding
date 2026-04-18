5a47b6884ff3f1241ad0b1c6487349b596e8d595
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-17 20:19:52 -0400

    done

--- character_replacement.py
+++ character_replacement.py
@@ -3,43 +3,38 @@ class Solution:
     def solve(self, s: str, k: int) -> int:
         from collections import defaultdict
 
-        l, r = 0, 0
-        replacementsLeft = k
-        maxLen = 0
-
         # The window length - most freq char in the window must be <= k for the window to be valid
         # Else we need to shift
-        window = ''
-        maxWindow = ''
+        l, r = 0, 0
+        maxWindow = 0
         freq = defaultdict(int)
 
         while r < len(s):
-            freq[r] += 1
-            mostFreq = max(freq.items(), key=lambda x: x[1])
+            freq[s[r]] += 1
+            mostFreq = max(freq.values())
+            # print(freq, mostFreq)
 
-            if s[r] == mostFreq[0]: # This is our targetChar we just expand
-                r += 1
-                continue
-
-            if len(s[l:r+1]) > len(maxWindow):
-                maxWindow = s[l:r+1]
-
-            while len(window[l:r+1]) - mostFreq[1] > k: # We have more diff chars than replacements, this window is invalid
-                freq[l] -= 1 # Remove from window
+            # We assume the window is valid unless proven otherwise
+            while r-l+1 - mostFreq > k: # We have more diff chars than replacements, this window is invalid
+                freq[s[l]] -= 1 # Remove from window
                 l += 1 # Shrink window until we are valid again
+            
+            if r-l+1 > maxWindow:
+                maxWindow = r-l+1
 
             r += 1
 
-        print(maxWindow)
-        return len(maxWindow)
+        # print(maxWindow)
+        return maxWindow
 
 def main():
     s = Solution()
-    # print(s.solve("ABAB", 2), 4)
+    print(s.solve("BABB", 1), 4)
+    print(s.solve("ABAB", 2), 4)
     print(s.solve("ABABBA", 2), 5)
-    # print(s.solve("AABABBA", 1), 4)
-    # print(s.solve("AABABBABB", 3), 8)
-    # print(s.solve("AAAB", 0), 3)
+    print(s.solve("AABABBA", 1), 4)
+    print(s.solve("AABABBABB", 3), 8)
+    print(s.solve("AAAB", 0), 3)
 
 
 if __name__ == '__main__':

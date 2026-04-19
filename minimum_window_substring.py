5cef42edd5041014a05698249a98020db688671a
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-19 16:59:52 -0400

    newgoal

--- minimum_window_substring.py
+++ minimum_window_substring.py
@@ -10,15 +10,29 @@ class Solution:
         freqT = Counter(t)
         print(freqT)
 
+        #  Expand while winow is invalid and r < length
+            # If window is valid
+            #     Save if smallest window then
+            #     Shrink until invalid
+
         l, r = 0, len(s)-1
+        windowTargetFreq = freqT # Number of letters still needed in the window
 
         # Shrink the window, we begin ASSUMING the entire string S is a valid window
         while l < r:
             # If both L and R are chars that occur in T then we will arbitrarily choose to shrink L
             #     (May need ot check which side to shrink keeps validity to pick)
+
             # If only L is in T we will shrink R
+            if s[l] in freqT:
+                r -= 1
             # If only R is in T we will shrink L
+            elif s[r] in freqT:
+                l += 1
             # Else neither are in and we will shrink BOTH
+            else:
+                l += 1
+                r -= 1
 
             # If windowFreq >= freqT
             #     This is a valid window, set it if smaller than current window        

0868a1a6e9d4eb7d5790eb721b5b285a3c953f22
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-19 16:26:52 -0400

    init

---
+++ minimum_window_substring.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/minimum-window-substring/description/
+class Solution:
+    def solve(self, s: str, t: str) -> str:
+        ...
+
+
+def main():
+    s = Solution()
+    print(s.solve("ADOBECODEBANC", "ABC"))
+
+if __name__ == '__main__':
+    main()

33389f2fd295dacdba2f19de73b7a98b88685766
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-19 16:43:04 -0400

    plan

--- minimum_window_substring.py
+++ minimum_window_substring.py
@@ -4,9 +4,27 @@ class Solution:
         # Smallest window where ALL charactes of t (incl. dupes) exist in s
         # No guarantee that this window will exist in s
 
-        
+        from collections import Counter
 
-        return ""
+        window = ''
+        freqT = Counter(t)
+        print(freqT)
+
+        l, r = 0, len(s)-1
+
+        # Shrink the window, we begin ASSUMING the entire string S is a valid window
+        while l < r:
+            # If both L and R are chars that occur in T then we will arbitrarily choose to shrink L
+            #     (May need ot check which side to shrink keeps validity to pick)
+            # If only L is in T we will shrink R
+            # If only R is in T we will shrink L
+            # Else neither are in and we will shrink BOTH
+
+            # If windowFreq >= freqT
+            #     This is a valid window, set it if smaller than current window        
+
+
+        return window if window != s else '' # No valid substring t in s
 
 
 def main():

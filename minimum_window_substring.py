44142da78ec8159236566b5e099a2d113500b87b
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-19 17:36:09 -0400

    consolidate stage 2 and done

--- minimum_window_substring.py
+++ minimum_window_substring.py
@@ -19,45 +19,47 @@ class Solution:
         windowFreq = freqT # Once each value is <=0 the window is valid
 
         while r < len(s):
-            # print(f'{s[l:r+1]}, {l=}, {r=}, {window=}, {windowFreq=}')
+            print(f'{s[l:r+1]}, {l=}, {r=}, {window=}, {windowFreq=}')
             # A window is invalid if sum of freqT > sum of windowFreq for eahc letter in freqT
             # This meanss not enough letters from t in the window exist to be a valid substring
-            while sum(windowFreq.values()) <= 0 and l <= r: # Window is valid now
-                # print('Window is valid')
-                if len(s[l:r+1]) < len(window): # New smallest substring
+            # We don't want to muddy our freq with chars not common to both strings
+            if s[r] in windowFreq:
+                windowFreq[s[r]] -= 1
+
+            # Max prevents ufixes issue where sum would be 0 from seeing multiple dupe letters but not ALL the required letters
+            while max(windowFreq.values()) <= 0 and l <= r: # Window is valid now
+                print('Window is valid', windowFreq)
+                if len(s[l:r+1]) <= len(window): # New smallest substring
+                    print('Replacing')
                     window = s[l:r+1]
                 
                 if s[l] in windowFreq:
                     windowFreq[s[l]] += 1
                 l += 1
             
-            # We don't want to muddy our freq with chars not common to both strings
-            if s[r] in windowFreq:
-                windowFreq[s[r]] -= 1
             r += 1
         
         # Two stages is simplest initially
-        # print('Stage Two')
+        print('Stage Two')
         while l < r:
-            # print(f'{s[l:r+1]}, {l=}, {r=}, {window=}, {windowFreq=}')
+            print(f'{s[l:r+1]}, {l=}, {r=}, {window=}, {windowFreq=}')
+            if s[l] in windowFreq:
+                windowFreq[s[l]] += 1
 
             if sum(windowFreq.values()) <= 0 and len(s[l:r]) < len(window): # Window is valid now
                 window = s[l:r+1]
                 
-            if s[l] in windowFreq:
-                windowFreq[s[l]] += 1
-            
             l += 1
 
-        return window if window != s else '' # No valid substring t in s
+        return window if max(windowFreq.values()) <= 0 else '' # No valid substring t in s
 
 
 def main():
     s = Solution()
-    print(s.solve("ADOBECODEBANC", "ABC"))
-    print(s.solve("ADOBECODEBANC", "DEDOB"))
-    print(s.solve("a", "a"))
-    print(s.solve("a", "aa"))
+    # print(s.solve("ADOBECODEBANC", "ABC"), 'BANC')
+    # print(s.solve("ADOBECODEBANC", "DEDOB"), 'DOBECOD')
+    print(s.solve("a", "a"), 'a')
+    # print(s.solve("a", "aa"), '')
 
 if __name__ == '__main__':
     main()

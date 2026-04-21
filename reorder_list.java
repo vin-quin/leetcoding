a356eb2115646cdf993dab8f3a5c95f0e0cfc26a
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-21 15:02:19 -0400

    init

---
+++ reorder_list.java
@@ -0,0 +1,16 @@
+/**
+ * https://neetcode.io/problems/reorder-linked-list/question?list=neetcode150
+ */
+class Solution {
+    public int solve(int[] nums) {
+        
+        return 0;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solve());
+    }
+}

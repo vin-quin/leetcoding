0da22f5172fa3a3e7b84e3bf7580e8ba4773850f
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-21 14:06:51 -0400

    setup

---
+++ merge_two_lists.java
@@ -0,0 +1,27 @@
+/**
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ */
+
+// Definition for singly-linked list.
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode solve(ListNode list1, ListNode list2) {
+        
+        
+        return new ListNode();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solve());
+    }
+}
--- reverse_list.py
+++ reverse_list.py
@@ -4,9 +4,59 @@ class ListNode:
         self.val = val
         self.next = next
 
+from typing import Optional
+
 class Solution:
     def solve(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        ...
+        if not head:
+            return head
+        
+        stack = []
+
+        node = head
+        while node:
+            stack.append(node)
+            node = node.next
+
+        print(stack)
+        n = stack.pop()
+
+        while stack:
+            nextN = stack.pop()
+
+            n.next = nextN
+            n = nextN
+
+        return n
+
+def rec(head: Optional[ListNode]):
+    if head:
+        n = rec(head.next)
+        return n
+    
+    head.next = rec(head.next)
+
+    return head
+
+def iter(head: Optional[ListNode]):
+    if not head:
+            return head
+        
+    stack = [None]
+
+    node = head
+    while node:
+        stack.append(node)
+        node = node.next
+
+    head = stack.pop()
+    node = head
+
+    while stack:
+        node.next = stack.pop()
+        node = node.next
+
+    return head
 
 
 def main():

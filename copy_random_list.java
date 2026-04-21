efb5bcd19ce831d766af777924efaa1070f54aba
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-21 19:13:38 -0400

    done

--- copy_random_list.java
+++ copy_random_list.java
@@ -3,7 +3,7 @@
  * https://leetcode.com/problems/copy-list-with-random-pointer/description/
  */
 
-import java.util.ArrayList;
+import java.util.HashMap;
 // Definition for a Node.
 
 public class Node {
@@ -20,16 +20,36 @@ public class Node {
 }
 
 public class Solution {
-
     public Node copyRandomList(Node head) {
-        ArrayList<Node> a = new ArrayList();
+        HashMap<Node,Node> nodeMap = new HashMap(); // Old:New
 
-        Node curr = head;
-        while (curr != null) {
+        // A map of old Node to new Node lets us iterate through O(N) the original and copy simultaneously 
+        // and set them as we go so 2 O(N) ops
 
+        Node deep = deepCopy(head, nodeMap);
+
+        Node orig = head;
+        Node copy = deep;
+
+        while (orig != null) {
+            copy.random = nodeMap.get(orig.random);
+
+            copy = copy.next;
+            orig = orig.next;
         }
 
-        return null;
+        return deep;
+    }
+
+    public Node deepCopy(Node root, HashMap<Node,Node> nodeMap) {
+        if (root == null) {
+            return null;
+        }
+
+        Node n = new Node(root.val);
+        n.next = deepCopy(root.next, nodeMap);
+        nodeMap.put(root, n);
+        return n;
     }
 }
 
@@ -37,6 +57,11 @@ public class Main {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solve());
+        Node node1=new Node(1);
+        Node node2=new Node(2);
+        node1.next = node2;
+        node1.random = node2;
+        node2.random = node2;
+        System.out.println(s.copyRandomList(node1));
     }
 }

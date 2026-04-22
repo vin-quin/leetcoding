48dd007cbe1c2186ae26ec501f13f5dd2939df47
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-22 19:45:20 -0400

    init

---
+++ add_two_numbers.java
@@ -0,0 +1,23 @@
+/**
+ * https://leetcode.com/problems/add-two-numbers/description/
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }   
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.addTwoNumbers(l1, l2)());
+    }
+}

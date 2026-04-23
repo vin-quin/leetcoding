8cf91248e54846335bd9149471d38941c413322b
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-23 17:01:58 -0400

    init

---
+++ find_duplicate_number.java
@@ -0,0 +1,16 @@
+/**
+ * https://leetcode.com/problems/find-the-duplicate-number/
+ */
+class Solution {
+    public int solve(int[] nums) {
+        
+        return 0;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solve());
+    }
+}

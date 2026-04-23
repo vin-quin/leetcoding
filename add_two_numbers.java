262c1baf581a9ef01f2f27fcd8d90ca104ffb8b4
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-22 20:33:54 -0400

    done

--- add_two_numbers.java
+++ add_two_numbers.java
@@ -12,54 +12,46 @@ public class ListNode {
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int[] nums1 = new int[9];
-        int[] nums2 = new int[9];
-
-        // Get the digits in each num, worst casse 9 iters
-        int i = 8;
+        ListNode resHead = new ListNode();
+        ListNode res = resHead;
+        int carry = 0;
+        
+        // We can just add each node we see with a carry bit if needed since we want the result as a list in the same format
         ListNode curr1 = l1, curr2 = l2;
         while (curr1 != null || curr2 != null) {
+            int n1=0,n2=0;
+
             if (curr1 != null) {
-                nums1[i] = curr1.val;
+                n1 = curr1.val;
                 curr1 = curr1.next;
             }
             
             if (curr2 != null) {
-                nums2[i] = curr2.val;
+                n2 = curr2.val;
                 curr2 = curr2.next;
             }
 
-            i--;
+            int s = n1+n2+carry;
+            if (s > 9) { // Remove the tens place and add as carry for next iter
+                s -= 10;
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+
+            ListNode digit = new ListNode(s);
+            res.next = digit;
+            res = res.next;
         }
 
-        System.out.println(nums1);
-        System.out.println(nums2);
-
-        // Convert nums to actual ints
-        int num1=0, num2=0;
-        int exp = 8;
-
-        // N * 10^exp = digit's real value for its pos
-        for (i = 0; i < nums1.length; i++) {
-            num1 += nums1[i] * Math.pow(10, exp);
-            exp--;
+        // We need to add a new node as the sum has a greater magnitude than its parts
+        if (carry != 0) {
+            res.next = new ListNode(carry);
         }
 
-        exp=8;
-        // N * 10^exp = digit's real value for its pos
-        for (i = 0; i < nums2.length; i++) {
-            num2 += nums2[i] * Math.pow(10, exp);
-            exp--;
-        }
-
-        System.out.println(num1);
-        System.out.println(num2);
-
-        int res = num1+num2;
-        ListNode resHead = new ListNode(0);
-        // Rebuild res as a list in same format and return that node
-
-        return resHead;
+        // We always start with a leading 0 at the head so just return next for the real result
+        return resHead.next;
+    }
     }
 }
 

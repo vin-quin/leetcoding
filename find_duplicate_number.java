40b2180bf881ad90d503387ca38b526d35858022
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-23 17:09:25 -0400

    done

--- find_duplicate_number.java
+++ find_duplicate_number.java
@@ -1,16 +1,29 @@
 /**
  * https://leetcode.com/problems/find-the-duplicate-number/
  */
+import java.util.Arrays;
 class Solution {
+    public Solution() {}
+
     public int solve(int[] nums) {
-        
-        return 0;
+        Arrays.sort(nums);
+        int lastSeen = 0;
+
+        for (int n : nums) {
+            if (n == lastSeen) {
+                return n;
+            }
+
+            lastSeen = n;
+        }
+
+        return -1;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solve());
+        System.out.println(s.solve(new int[]{1,3,4,2,2}));
     }
 }

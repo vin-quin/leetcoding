41c084674638d4818d4e65ca08c914e5096c2dec
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-22 20:07:29 -0400

    would work final step gets complicated

--- add_two_numbers.java
+++ add_two_numbers.java
@@ -1,6 +1,7 @@
 /**
  * https://leetcode.com/problems/add-two-numbers/description/
  */
+ import java.lang.Math;
 public class ListNode {
     int val;
     ListNode next;
@@ -11,8 +12,55 @@ public class ListNode {
 
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
-    }   
+        int[] nums1 = new int[9];
+        int[] nums2 = new int[9];
+
+        // Get the digits in each num, worst casse 9 iters
+        int i = 8;
+        ListNode curr1 = l1, curr2 = l2;
+        while (curr1 != null || curr2 != null) {
+            if (curr1 != null) {
+                nums1[i] = curr1.val;
+                curr1 = curr1.next;
+            }
+            
+            if (curr2 != null) {
+                nums2[i] = curr2.val;
+                curr2 = curr2.next;
+            }
+
+            i--;
+        }
+
+        System.out.println(nums1);
+        System.out.println(nums2);
+
+        // Convert nums to actual ints
+        int num1=0, num2=0;
+        int exp = 8;
+
+        // N * 10^exp = digit's real value for its pos
+        for (i = 0; i < nums1.length; i++) {
+            num1 += nums1[i] * Math.pow(10, exp);
+            exp--;
+        }
+
+        exp=8;
+        // N * 10^exp = digit's real value for its pos
+        for (i = 0; i < nums2.length; i++) {
+            num2 += nums2[i] * Math.pow(10, exp);
+            exp--;
+        }
+
+        System.out.println(num1);
+        System.out.println(num2);
+
+        int res = num1+num2;
+        ListNode resHead = new ListNode(0);
+        // Rebuild res as a list in same format and return that node
+
+        return resHead;
+    }
 }
 
 public class Main {

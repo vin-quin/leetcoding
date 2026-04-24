52f6bc51c1e62357f2f9da762a7c3df10cca1e83
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-24 13:09:34 -0400

    iter

--- merge_k_sorted_lists.java
+++ merge_k_sorted_lists.java
@@ -22,34 +22,46 @@ class Solution {
         // We will merge into this list after we initialize this current lists kv
         this.head = lists[0]; // Our sorted list starts as our first lists arbitrarily
         ListNode curr = this.head;
-        do  {
+        while (curr != null)  {
             this.kv.put(curr.val, curr);
-        } while (curr.next != null);
+            
+            if (curr.next == null) {
+                this.tail = curr;
+            }
 
-
+            curr = curr.next;
+        } 
+        
         System.out.println(this.kv);
 
         for (int i = 1; i < lists.length; i++) { // Skip first list we started at
             curr = lists[i];
+            System.out.println(i);
 
             while (curr != null) {
                 // If we've seen this number in the kv then we need to insert it somewhere inside and do a swap
                 if (this.kv.containsKey(curr.val)) {
-                    this.insert(curr);
+                    System.out.println("Contains");
+                    this.insert(new ListNode(curr.val));
                 } else if (curr.val < this.head.val) { // Else if it is less than head it must be new head
+                    System.out.println("New head");
                     ListNode tmp = new ListNode(curr.val);
                     tmp.next = this.head;
                     this.head = tmp;
+                    this.kv.put(this.head.val, this.head);
                 } else { // Else it must be new tail
+                    System.out.println("New tail");
                     ListNode tmp = new ListNode(curr.val);
                     this.tail.next = tmp;
                     this.tail = tmp;
+                    this.kv.put(this.tail.val, this.tail);
                 }
 
                 curr = curr.next;
             }
         }
-
+        
+        System.out.println(this.kv);
         return this.head;
     }
 
@@ -57,6 +69,8 @@ class Solution {
         ListNode insertAt = this.kv.get(n.val);
 
         n.next = insertAt.next;
-        insertAt.next = n;        
+        insertAt.next = n;   
+
+        this.kv.put(n.val, n);     
     }
 }

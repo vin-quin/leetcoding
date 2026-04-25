d9ad87f005600a45252082661249e83eb0e9440f
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-25 17:53:58 -0400

    init

---
+++ 572_subtree.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/subtree-of-another-tree/description/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        
+    }
+}

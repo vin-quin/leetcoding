4d0af02a9d2e917100adff6b723a8fe703196fd3
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-25 18:24:06 -0400

    close

--- 572_subtree.java
+++ 572_subtree.java
@@ -1,4 +1,5 @@
 // https://leetcode.com/problems/subtree-of-another-tree/description/
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -15,7 +16,41 @@
  * }
  */
 class Solution {
+    private boolean found = false;
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        
+        subtree(root, subRoot);
+        return this.found;
+    }
+
+    public boolean subtree(TreeNode root, TreeNode subtree) {
+        if ((root == null && subtree != null) || root != null && subtree == null) {
+            return false;
+        }
+
+        if (root == null && subtree == null) {
+            return true;
+        }
+
+        // Potential subtree, explore
+        if (root.val == subtree.val) {
+            this.found = same(root, subtree);
+        }
+
+        subtree(root.left, subtree);
+        subtree(root.right, subtree);
+
+        return false;
+    }
+
+    public boolean same(TreeNode a, TreeNode b) {
+        if (a == null && b == null) {
+            return true;
+        }
+
+        if ((a == null && b != null) || a != null && b == null || (a.val != b.val)) {
+            return false;
+        }
+
+        return (same(a.left, b.left) == true && same(a.right, b.right) == true);
     }
 }

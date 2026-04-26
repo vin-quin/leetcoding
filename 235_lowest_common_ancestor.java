c67f3cc7b9c36b906b3bf4604edd6770abe0bc49
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-25 21:13:56 -0400

    init

---
+++ 235_lowest_common_ancestor.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+    }
+}

079a74c4bb2c395cd0c93b84773c7c2495f1b304
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-25 22:22:55 -0400

    done

--- 235_lowest_common_ancestor.java
+++ 235_lowest_common_ancestor.java
@@ -3,8 +3,8 @@
 /**
  * Definition for a binary tree node.
  */
-import java.util.Stack;
 
+import java.util.Stack;
 public class TreeNode {
 
     int val;
@@ -21,36 +21,28 @@ class Solution {
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         Stack<TreeNode> s1 = new Stack<>();
-        dfs(root, p, s1);
         Stack<TreeNode> s2 = new Stack<>();
+        dfs(root, p, s1);
         dfs(root, q, s2);
-
         s1.retainAll(s2);
 
-        for (TreeNode elem : s1) {
-            System.out.println(elem.val);
-        }
-
-        return s1.firstElement();
+        return s1.lastElement();
     }
 
-    public void dfs(TreeNode root, TreeNode t, Stack<TreeNode> history) {
+    public void dfs(TreeNode root, TreeNode target, Stack<TreeNode> history) {
         if (root == null) {
-            history.pop();
             return;
         }
 
-        history.push(root);
-        if (root == t) {
+        history.add(root);
+        if (root == target) {
             return;
         }
 
-        if (root.left != null) {
-            dfs(root.left, t, history);
-        }
-
-        if (root.right != null) {
-            dfs(root.right, t, history);
+        if (target.val < root.val) {
+            dfs(root.left, target, history);
+        } else {
+            dfs(root.right, target, history);
         }
     }
 }

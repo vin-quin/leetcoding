70ffd5e0c03d6e00e14bef88bc4b007df253aaf8
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-27 19:53:06 -0400

    done

--- 98_valid_binary_tree.java
+++ 98_valid_binary_tree.java
@@ -1,11 +1,20 @@
 // https://leetcode.com/problems/validate-binary-search-tree/description/
 // Definition for a binary tree node.
+
+import java.lang.Long;
 public class TreeNode {
+
     int val;
     TreeNode left;
     TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
     TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
@@ -13,8 +22,20 @@ public class TreeNode {
     }
 }
 
-class Solution {
-    public boolean isValidBST(TreeNode root) {
-        
+    class Solution {
+
+        public boolean isValidBST(TreeNode root) {
+            return valid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        }
+
+        public boolean valid(TreeNode root, long min, long max) {
+            if (root == null) {
+                return true;
+            }
+            if (!(min < root.val && root.val < max)) { // Not in range
+                return false;
+            }
+
+            return valid(root.left, min, root.val) && valid(root.right, root.val, max);
+        }
     }
-}

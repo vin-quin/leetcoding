eb839895d00eae9372fd156b855b654e23b974a8
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-27 16:07:16 -0400

    init

---
+++ 98_valid_binary_tree.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/validate-binary-search-tree/description/
+// Definition for a binary tree node.
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        
+    }
+}

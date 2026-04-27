db52540060db0eaa93fdf8a87994c417cedb1e73
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-27 15:45:32 -0400

    init

---
+++ 199_right_side_view.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/binary-tree-right-side-view/
+// Definition for a binary tree node.
+import java.util.List;
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        
+    }
+}

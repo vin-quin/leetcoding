03bd5986467e95d1169e3cd6fcfa472b8bc08f52
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-28 14:20:09 -0400

    inti

---
+++ 78_subsets.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/subsets/description/
+class Solution {
+    public List<List<Integer>> solve(int[] nums) {
+        
+        return 0;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solve(new int[]{1,2,3}));
+    }
+}

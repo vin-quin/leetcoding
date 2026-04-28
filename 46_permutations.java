cb7c74d1169a54833f359576f8e05792d8c86191
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-28 15:44:20 -0400

    done

--- 46_permutations.java
+++ 46_permutations.java
@@ -1,21 +1,43 @@
+
 /**
  * https://leetcode.com/problems/permutations/description/
  */
 import java.util.ArrayList;
 import java.util.List;
-class Solution {
-    public List<List<Integer>> solve(int[] nums) {
-        List<List<Integer>> r = new ArrayList<>();
-        
 
-        
-        return r;
+class Solution {
+
+    public List<List<Integer>> solve(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+        backtrack(nums, res, new ArrayList<Integer>(), used);
+        return res;
+    }
+
+    public void backtrack(int[] nums,  List<List<Integer>> res, List<Integer> perm, boolean[] used) {
+        if (perm.size() == nums.length) {
+            res.add(new ArrayList<>(perm));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                perm.add(nums[i]);
+                backtrack(nums, res, perm, used);
+
+                perm.remove(perm.size() - 1); // Remove the last number we added in the permutation and go next
+                used[i] = false;
+            }
+        }
+
     }
 }
 
 public class Main {
+
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solve(new int[]{1,2,3}));
+        System.out.println(s.solve(new int[]{1, 2, 3}));
     }
 }

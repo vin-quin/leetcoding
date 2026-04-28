1bafb18ba7d7a6221ccae70e734fc4ada6f8858a
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-28 16:24:40 -0400

    iterative

--- 133_clone_graph.java
+++ 133_clone_graph.java
@@ -1,6 +1,10 @@
 /* 
 https://leetcode.com/problems/clone-graph/
 */
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashSet;
 class Node {
     public int val;
     public List<Node> neighbors;
@@ -20,6 +24,43 @@ class Node {
 
 class Solution {
     public Node cloneGraph(Node node) {
-        
+
+
+        return walk(node);
     }
+
+    public Node walk(Node original) {
+        Node copy = new Node(original.val);
+        ArrayList<Node> q = new ArrayList<Node>(original);
+        ArrayList<Node> copyQ = new ArrayList<Node>(copy);
+        HashSet<Integer> seen = new HashSet<>();
+
+        while (!q.isEmpty()) {
+            Node o = q.removeFirst();
+            Node c = copyQ.removeFirst();
+            
+            if (seen.contains(o.val)) {
+                continue;
+            }
+
+            seen.add(o.val);
+
+            for (Node neighbor : o.neighbors) {
+                Node n = new Node(neighbor.val);
+                c.neighbors.add(n);
+                q.add(neighbor);
+                copyQ.add(n);
+            }
+        }
+
+        return copy;
+    }
+
+    // public void copy(Node original, Node copy) {
+    //     copy = new Node(original.val);
+
+    //     for (Node n: original.neighbors) {
+    //         copy(n, )
+    //     }
+    // }
 }

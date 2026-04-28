f264e8bd737bab524380b4e3fafde7e233444a58
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-28 14:57:41 -0400

    iinit

---
+++ 46_permutations.java
@@ -0,0 +1,21 @@
+/**
+ * https://leetcode.com/problems/permutations/description/
+ */
+import java.util.ArrayList;
+import java.util.List;
+class Solution {
+    public List<List<Integer>> solve(int[] nums) {
+        List<List<Integer>> r = new ArrayList<>();
+        
+
+        
+        return r;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solve(new int[]{1,2,3}));
+    }
+}

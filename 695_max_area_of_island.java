fdbfa11a759436b677394a06728ff47115812e87
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-28 16:03:52 -0400

    done

--- 695_max_area_of_island.java
+++ 695_max_area_of_island.java
@@ -3,7 +3,25 @@
  */
 class Solution {
     public int solve(int[][] grid) {
-        
-        return 0;
+        int area = 0;
+
+        for (int r =0; r < grid.length; r++) {
+            for (int c=0; c < grid[0].length; c++) {
+                if (grid[r][c] == 1) {
+                    area = Math.max(area, flood(grid, r, c));
+                }
+            }
+        }
+
+        return area;
+    }
+
+    public int flood(int[][] grid, int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] != 1) {
+            return 0;
+        }
+
+        grid[row][col] = -1; // Remove so we dont recount
+        return 1+flood(grid, row-1,col)+flood(grid,row+1,col)+flood(grid,row,col-1)+flood(grid,row,col+1);
     }
 }

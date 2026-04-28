9a28f0be907f70718fa152415082fe6b910c6dbd
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-28 15:53:18 -0400

    done

--- 200_num_islands.java
+++ 200_num_islands.java
@@ -1,11 +1,41 @@
 /**
  * https://leetcode.com/problems/number-of-islands/description/
  */
+
 class Solution {
     public int solve(char[][] grid) {
-        
+        int islands = 0;
 
-        
-        return 0;
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                if (grid[row][col] == '1') {
+                    islands++;
+                    flood(grid, row, col);
+                }
+            }
+        }
+
+        return islands;
+    }
+
+    // Flood an island to get it off the map
+    public void flood(char[][] grid, int row, int col) {
+        if (row < 0 || row >= grid.length) {
+            return;
+        }
+
+        if (col < 0 || col >= grid[0].length) {
+            return;
+        }
+
+        if (grid[row][col] != '1') {
+            return;
+        }
+
+        grid[row][col] = '0';
+        flood(grid, row+1, col);
+        flood(grid, row-1, col);
+        flood(grid, row, col-1);
+        flood(grid, row, col+1);
     }
 }

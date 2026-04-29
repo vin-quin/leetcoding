6aca2aecf657b6641d2816d5a4b574e0a4c2f56c
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-29 18:37:06 -0400

    doneboth

--- 206_reverse_linked_list.java
+++ 206_reverse_linked_list.java
@@ -1,1 +1,46 @@
 // https://leetcode.com/problems/reverse-linked-list/description/
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        return head != null ? rec(head) : null;
+    }
+
+    public ListNode rec(ListNode root) {
+        if (root.next == null) {
+            return root;
+        }
+
+        ListNode n = rec(root.next);
+        root.next.next = root;
+        root.next = null; 
+
+        return n;
+    }
+
+    public ListNode iter(ListNode root) {
+        Stack<ListNode> stk = new Stack<>();
+
+        ListNode curr = root;
+        while (curr != null) {
+            stk.push(curr);
+            curr = curr.next;
+        }
+
+        if (!stk.isEmpty()) {
+            root = stk.pop();
+        }
+
+        curr = root;
+        while (!stk.isEmpty()) {
+            ListNode n = stk.pop();
+
+            curr.next = n;
+            curr = curr.next;
+        }
+
+        if (curr != null) {
+            curr.next = null;
+        }
+
+        return root;
+    }
+}

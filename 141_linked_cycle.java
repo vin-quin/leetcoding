1c4a95e69d683c9d52d848619efea7c0596b52df
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-29 18:52:58 -0400

    optimal

--- 141_linked_cycle.java
+++ 141_linked_cycle.java
@@ -1,1 +1,22 @@
 // https://leetcode.com/problems/linked-list-cycle/description/
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        ListNode slow=head,fast=head;
+
+        while (slow.next != null && fast.next != null && fast.next.next != null) {
+            // Eventually fast will reach slow if there is a cycle, otherwise we will hit a dead end
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}

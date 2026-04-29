76e78350eccef4934cc5790b23bdfd7b1f4bb423
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-29 01:31:42 -0400

    betterdone

--- 22_generate_parentheses.java
+++ 22_generate_parentheses.java
@@ -1,31 +1,25 @@
 // https://leetcode.com/problems/generate-parentheses/description/
 import java.util.List;
 import java.util.ArrayList;
-import java.lang.StringBuilder;
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<String>();
-        backtrack(n, new StringBuilder(), 0, 0, res);
+        backtrack(n, "", 0, 0, res);
         return res;
     }
 
     public void backtrack(int n, StringBuilder s, int close, int open, List<String> res) {
         if (s.length() == 2*n) { //A valid paren pair stirng will always be length 2n
-            res.add(s.toString());
+            res.add(s);
             return;
         }
 
         if (open < n) {
-            s.append('(');
-            backtrack(n, s, close, open++, res);
-            s.deleteCharAt(s.length()-1);
+            backtrack(n, s+'(', close, open+1, res);
         }
 
         if (close < open) {
-            s.append(')');
-            close++;
-            backtrack(n, s, close++, open, res);
-            s.deleteCharAt(s.length()-1);
+            backtrack(n, s+')', close+1, open, res);
         }
     }
 }

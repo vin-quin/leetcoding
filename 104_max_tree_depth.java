a1a51a259b2c407a83b48fa270bf3e8b0b38d777
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-29 18:59:22 -0400

    done

--- 104_max_tree_depth.java
+++ 104_max_tree_depth.java
@@ -1,1 +1,14 @@
 // https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+class Solution {
+    public int maxDepth(TreeNode root) {
+        return dfs(root);
+    }
+
+    public int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(dfs(root.left), dfs(root.right))+1;
+    }
+}

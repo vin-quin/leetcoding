c5517ca96a2693f791d89be99c7b6def958c6a80
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-29 16:16:48 -0400

    done

--- 283_move_zeroes.java
+++ 283_move_zeroes.java
@@ -1,1 +1,26 @@
 // https://leetcode.com/problems/move-zeroes/description/
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if (nums.length == 1) {
+            return;
+        }
+
+        // Immediately put nonzeroes at the insertIdx andwe just append htme at the end
+        int insertAt = 0;
+        int zeroes = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[insertAt] = nums[i];
+                insertAt++; // Move to maintain order
+            } else {
+                zeroes++;
+            }
+        }
+
+        for (int i=0; i < zeroes; i++) {
+            nums[insertAt] = 0;
+            insertAt++;
+        }
+    }
+}

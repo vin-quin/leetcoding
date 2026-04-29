82770644c387381b2cf85562e6c88e074e5d448d
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-29 18:12:15 -0400

    done

--- 121_max_profit.java
+++ 121_max_profit.java
@@ -1,1 +1,21 @@
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 1) {
+            return 0;
+        } 
+
+        int profit = 0;
+        int buy = prices[0];
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            } else {
+                profit = Math.max(profit, prices[i]-buy);
+            }
+        }
+
+        return profit;
+    }
+}

fad76182362cd187f8ceaa2910e32df9d8d3e223
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-29 15:57:23 -0400

    done

--- 14_longest_common_prefix.java
+++ 14_longest_common_prefix.java
@@ -1,1 +1,18 @@
 // https://leetcode.com/problems/longest-common-prefix/
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            while (!prefix.isEmpty() && !strs[i].startsWith(prefix)) { // Remove a letter until valid or we empty
+                prefix = String.copyValueOf(prefix.toCharArray(), 0, prefix.length()-1);
+            }
+
+            if (prefix.isEmpty()) { // No valid prefixes exist
+                return prefix;
+            }
+        }
+
+        return prefix;
+    }
+}

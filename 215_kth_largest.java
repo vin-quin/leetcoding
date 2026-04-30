80830dfe83e5db47ece4b9aeca83a2880717ada1
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-29 21:07:46 -0400

    done

--- 215_kth_largest.java
+++ 215_kth_largest.java
@@ -1,1 +1,17 @@
 // https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> q = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+
+        for (int n: nums) {
+            q.add(n);
+        }
+
+        int n=0;
+        for (int i = 0; i < k; i++){
+            n = q.poll();
+        }
+
+        return n;
+    }
+}

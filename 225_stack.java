b78ab0c1c8ef7672c1d82bc36afaa2d4dbe4d060
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-04-29 20:41:18 -0400

    done

--- 225_stack.java
+++ 225_stack.java
@@ -1,1 +1,33 @@
 // https://leetcode.com/problems/implement-stack-using-queues/description/
+class MyStack {
+    private ArrayDeque<Integer> q;
+
+    public MyStack() {
+        this.q = new ArrayDeque<Integer>();
+    }
+    
+    public void push(int x) {
+        this.q.addFirst(x);
+    }
+    
+    public int pop() {
+        return this.q.pollFirst();
+    }
+    
+    public int top() {
+        return this.q.peekFirst();
+    }
+    
+    public boolean empty() {
+        return this.q.size() == 0;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */

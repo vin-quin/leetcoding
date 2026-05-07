eaf912b2900776ed26605ecb19569329a20350bb
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-05-07 16:40:57 -0400

    init

---
+++ 78_subsets.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/subsets/description/
+class Solution:
+    def solve(self, nums: list[int]) -> list[list[int]]:
+        ...
+
+
+def main():
+    s = Solution()
+    print(s.solve([1,2,3]))
+
+if __name__ == '__main__':
+    main()

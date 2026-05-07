cada69a5fd013bb8f9021053ead8c05b8a628d9f
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-05-07 17:00:08 -0400

    init

---
+++ 40_combination_sum_2.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/combination-sum-ii/description/
+# 
+class Solution:
+    def combinationSum2(self, candidates: list[int], target: int) -> list[list[int]]:
+        res = []
+        return res
+
+
+def main():
+    s = Solution()
+    print(s.combinationSum2([10,1,2,7,6,1,5], 8))
+
+if __name__ == '__main__':
+    main()

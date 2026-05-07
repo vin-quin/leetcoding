b907129f2bc32237ee6bf14ae20826967e66c465
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-05-07 16:57:21 -0400

    done

--- 78_subsets.py
+++ 78_subsets.py
@@ -1,7 +1,21 @@
 # https://leetcode.com/problems/subsets/description/
 class Solution:
     def solve(self, nums: list[int]) -> list[list[int]]:
-        ...
+        subsets = []
+        backtrack(nums, 0, [], subsets)
+
+        return subsets
+        
+def backtrack(nums: list[int], i: int, thisSet: list[int], subsets: list[list[int]]):
+    if i >= len(nums):
+        subsets.append([*thisSet])
+        return
+    
+    thisSet.append(nums[i])
+    backtrack(nums, i+1, thisSet, subsets)
+    thisSet.pop()
+
+    backtrack(nums, i+1, thisSet, subsets)
 
 
 def main():

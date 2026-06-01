0b43a0b74a42f702e5bd07f84cd3d0148dbbbee9
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-05-31 22:06:21 -0400

    pause

--- 40_combination_sum_2.py
+++ 40_combination_sum_2.py
@@ -3,12 +3,32 @@
 class Solution:
     def combinationSum2(self, candidates: list[int], target: int) -> list[list[int]]:
         res = []
+        backtrack(candidates, target, 0, [], 0, res)
+
         return res
+
+def backtrack(nums: list[int], target: int, idx: int, curr: list[int], currSum: int, res: list[list[int]]):
+    if currSum == target:
+        curr.sort()
+        if curr not in res:
+            res.append([*curr]) 
+        return
+    
+    if idx == len(nums):
+        return
+
+    curr.append(nums[idx])
+    currSum += nums[idx]
+    backtrack(nums, target, idx+1, curr, currSum, res)
+    currSum -= curr.pop()
+    
+    backtrack(nums, target, idx+1, curr, currSum, res)
 
 
 def main():
     s = Solution()
     print(s.combinationSum2([10,1,2,7,6,1,5], 8))
+    print(s.combinationSum2([2,5,2,1,2], 5))
 
 if __name__ == '__main__':
     main()

d05722a54cad69ed725e2c242653078c2d3ad356
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-06-02 00:20:25 -0400

    done

--- 973_k_closest_points.py
+++ 973_k_closest_points.py
@@ -1,9 +1,14 @@
 # https://leetcode.com/problems/k-closest-points-to-origin/description/
-# 
+from math import sqrt
+import heapq 
+
 class Solution:
     def kClosest(self, points: list[list[int]], k: int) -> list[list[int]]:
-        ...
+        dists = [[sqrt((p[0])**2 + (p[1])**2), p] for p in points]
 
+        return [v[1] for v in heapq.nsmallest(k, dists, key=lambda x: x[0])]
+
+        # return [heapq.heappop(dists) for i in range(k)]
 
 def main():
     s = Solution()

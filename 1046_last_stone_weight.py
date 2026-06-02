0c8e2736b1c5857854f327445f3a89ae72a76825
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-06-01 23:59:20 -0400

    done

--- 1046_last_stone_weight.py
+++ 1046_last_stone_weight.py
@@ -1,4 +1,15 @@
 # https://leetcode.com/problems/last-stone-weight/description/
+import heapq
+
 class Solution:
     def lastStoneWeight(self, stones: list[int]) -> int:
-        
+        heapq.heapify_max(stones)
+
+        while len(stones) > 1:
+            y, x = heapq.heappop_max(stones), heapq.heappop_max(stones)
+
+            if x != y:
+                y -= x
+                heapq.heappush_max(stones, y)
+
+        return stones[0] if stones else 0

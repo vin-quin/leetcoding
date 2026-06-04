05d0f531fb9086d55e11262cf08afdc27656aa12
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-06-04 15:09:13 -0400

    init

---
+++ 215_kth_largest.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+class Solution:
+    def solve(self, nums: list[int], k: int) -> int:
+        ...
+
+
+def main():
+    s = Solution()
+    print(s.solve([3,2,1,5,6,4], 2))
+
+if __name__ == '__main__':
+    main()

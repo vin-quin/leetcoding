b520c994d3a4f65829eedd9dba3375b462899b49
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-06-04 15:14:22 -0400

    init

---
+++ 621_task_scheduler.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/task-scheduler/description/
+class Solution:
+    def solve(self, tasks: list[str], n: int) -> int:
+        ...
+
+
+def main():
+    s = Solution()
+    print(s.solve(["A","A","A","B","B","B"], 2))
+
+if __name__ == '__main__':
+    main()

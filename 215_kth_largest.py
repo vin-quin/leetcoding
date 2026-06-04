4ed796a566b0367bd3dbd027dde44a0c96f094dc
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-06-04 15:10:57 -0400

    done

--- 215_kth_largest.py
+++ 215_kth_largest.py
@@ -1,7 +1,10 @@
 # https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+import heapq 
+
 class Solution:
     def solve(self, nums: list[int], k: int) -> int:
-        ...
+        heapq.heapify(nums)
+        return heapq.nlargest(k, nums)[-1]
 
 
 def main():

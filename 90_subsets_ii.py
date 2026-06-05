4fdc3aa6101ef46f91cb0d08bb993fbf8b9b7e2a
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-06-05 17:11:30 -0400

    done

--- 90_subsets_ii.py
+++ 90_subsets_ii.py
@@ -2,7 +2,28 @@
 # 
 class Solution:
     def solve(self, nums: list[int]) -> list[list[int]]:
-        return []
+        res = []
+        nums.sort()
+        
+        for i in range(len(nums)):
+            self.backtrack(nums, [], res, i)
+            
+        return res
+
+    def backtrack(self, nums: list[int], pwr: list[int], res: list[list[int]], idx: int):
+        if idx >= len(nums): # done
+            # res.append()
+            return 
+        
+        if pwr not in res:
+            res.append(pwr.copy())
+        pwr.append(nums[idx])
+        
+        if pwr not in res:
+            res.append(pwr.copy())
+
+        self.backtrack(nums, pwr, res, idx+1)
+        pwr.pop()
 
 
 def main():

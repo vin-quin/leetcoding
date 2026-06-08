e7b6aba684d043ccc60379ad7c565a894dc57d83
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-06-07 22:06:04 -0400

    done

--- 70_climbing_stairs.py
+++ 70_climbing_stairs.py
@@ -2,8 +2,15 @@
 # 
 class Solution:
     def solve(self, n: int) -> int:
-        ...
-
+        return rec(n)
+        
+def rec(n: int) -> int:
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 1
+    
+    return rec(n-2) + rec(n-1)
 
 def main():
     s = Solution()

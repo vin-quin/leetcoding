7c27cc395e35c05342a4d93ae6bb90613addf873
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-06-07 21:42:46 -0400

    init

---
+++ 70_climbing_stairs.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/climbing-stairs/description/
+# 
+class Solution:
+    def solve(self, n: int) -> int:
+        ...
+
+
+def main():
+    s = Solution()
+    print(s.solve(2))
+
+if __name__ == '__main__':
+    main()

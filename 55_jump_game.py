fab68c099306f32cf0a9fa7b8d4c498edabc244b
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-06-07 22:30:25 -0400

    init

---
+++ 55_jump_game.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/jump-game/description/
+# 
+class Solution:
+    def solve(self, nums: list[int]) -> bool:
+        ...
+
+
+def main():
+    s = Solution()
+    print(s.solve([2,3,1,1,4]))
+
+if __name__ == '__main__':
+    main()

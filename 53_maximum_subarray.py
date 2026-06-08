e362654fbcf2be45d6ba48dcb567a46e095cb1ad
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-06-07 22:10:40 -0400

    init

---
+++ 53_maximum_subarray.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/maximum-subarray/description/
+class Solution:
+    def solve(self, nums: list[int]) -> int:
+        ...
+
+
+def main():
+    s = Solution()
+    print(s.solve([-2,1,-3,4,-1,2,1,-5,4]))
+
+if __name__ == '__main__':
+    main()

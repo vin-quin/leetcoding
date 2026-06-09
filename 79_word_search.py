3e1c7635bd11b428b1c4fa5496073ed36abedb36
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-06-09 18:37:32 -0400

    done

--- 79_word_search.py
+++ 79_word_search.py
@@ -6,7 +6,7 @@ class Solution:
     def exist(self, board: list[list[str]], word: str) -> bool:
         for row in range(len(board)):
             for col in range(len(board[0])):
-                if self.checkNeighbor(deepcopy(board), [row,col], word, 0):
+                if self.checkNeighbor(board, [row,col], word, 0):
                     return True
 
         return False
@@ -16,23 +16,24 @@ class Solution:
     ) -> bool:
         r, c = pos[0], pos[1]
 
-        if idx >= len(word):  # Reached end of word successuflly
+        if idx == len(word):  # Reached end of word successuflly
             return True
 
         if r >= len(board) or r < 0 or c >= len(board[0]) or c < 0 or board[r][c] != word[idx]:
             return False
 
         board[r][c] = '0'
-
-        return (
+        res =  (
             self.checkNeighbor(board, [r, c + 1], word, idx + 1)
             or self.checkNeighbor(board, [r, c - 1], word, idx + 1)
             or self.checkNeighbor(board, [r - 1, c], word, idx + 1)
             or self.checkNeighbor(board, [r + 1, c], word, idx + 1)
         )
+        board[r][c] = word[idx]
 
-["A","A","A"],
-["B","C","D"]
+        return res
+
 s = Solution()
 # print(s.exist([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "ABCCED"))
 print(s.exist([["A","A","A"],["B","C","D"]], "AAB"))
+print(s.exist([["A","B","C","E"],["S","F","E","S"],["A","D","E","E"]], "ABCESEEEFS"))

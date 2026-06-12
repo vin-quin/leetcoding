b0e1850d0eacced55001abfde30f42cd174197c8
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-06-12 01:36:26 -0400

    init

---
+++ 1071_gca_of_strings.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/greatest-common-divisor-of-strings/description/
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        ...
+
+
+def main():
+    s = Solution()
+    print(s.gcdOfStrings("ABCABC", "ABC"))
+
+if __name__ == '__main__':
+    main()

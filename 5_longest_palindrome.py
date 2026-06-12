6edc02ecf7101c5d5fdebb40d1c171e80537ac30
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-06-12 01:35:12 -0400

    done

--- 5_longest_palindrome.py
+++ 5_longest_palindrome.py
@@ -1,7 +1,26 @@
 # https://leetcode.com/problems/longest-palindromic-substring/description/
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        ...
+        palindrome = ""
+
+        for i in range(len(s)):
+            # Assume odd
+            l, r = i, i
+            while l >= 0 and r  < len(s) and s[l] == s[r]:
+                if (r-l+1) > len(palindrome):
+                    palindrome = s[l:r+1]
+                l -= 1
+                r += 1
+
+            # Assume event
+            l, r = i, i+1
+            while l >= 0 and r  < len(s) and s[l] == s[r]:
+                if (r-l+1) > len(palindrome):
+                    palindrome = s[l:r+1]
+                l -= 1
+                r += 1
+
+        return palindrome
 
 s = Solution()
 print(s.longestPalindrome("babad"))

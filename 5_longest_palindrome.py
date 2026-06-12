622442cd971c59649f93d3d16fc5534b8047ba63
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-06-12 01:17:07 -0400

    init

---
+++ 5_longest_palindrome.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/longest-palindromic-substring/description/
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        ...
+
+s = Solution()
+print(s.longestPalindrome("babad"))

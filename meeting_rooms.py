8f996a1b1fc70e1b1207a4bccaba5bc133242b81
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-06-12 02:10:37 -0400

    init

---
+++ meeting_rooms.py
@@ -0,0 +1,18 @@
+# https://neetcode.io/problems/meeting-schedule/question?list=neetcode250
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: list[Interval]) -> bool:
+        intervals.sort(key=lambda x: x.start)
+
+        for i in range(1, len(intervals)):
+            if intervals[i-1].end > intervals[i].start:
+                return False
+
+        return True

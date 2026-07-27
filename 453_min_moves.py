62242934be749c9421db6651290eccf9cca2d981
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-07-27 17:56:51 -0400

    init

---
+++ 453_min_moves.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minMoves(self, nums: list[int]) -> int:
+        
+
+def main():
+    s = Solution()
+    print(s.minMoves([1,2,3]))
+
+if __name__ == '__main__':
+    main()

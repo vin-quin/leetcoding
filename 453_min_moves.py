97aec2c4d42e84a12900fe211495c10badab2557
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-07-27 18:09:09 -0400

    done

--- 453_min_moves.py
+++ 453_min_moves.py
@@ -1,10 +1,11 @@
 class Solution:
     def minMoves(self, nums: list[int]) -> int:
-        
+        return sum(nums) - len(nums) * min(nums)
 
 def main():
     s = Solution()
     print(s.minMoves([1,2,3]))
+    print(s.minMoves([-1,1,1]))
 
 if __name__ == '__main__':
     main()

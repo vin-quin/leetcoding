eb784cc69501b9afa4077ac4bf4ecb53e49219c6
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-07-27 18:09:50 -0400

    init

---
+++ 1689_min_partitions.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/description/
+class Solution:
+    def solve(self, n: str) -> int:
+        
+
+
+def main():
+    s = Solution()
+    print(s.solve())
+
+if __name__ == '__main__':
+    main()

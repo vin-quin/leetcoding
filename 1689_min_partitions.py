73aafe813660345d88f986b86b3d3a3e89b5277b
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-07-27 18:33:27 -0400

    done

--- 1689_min_partitions.py
+++ 1689_min_partitions.py
@@ -1,12 +1,20 @@
 # https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/description/
 class Solution:
     def solve(self, n: str) -> int:
-        
+        steps = 0
 
+        for d in n:
+            steps = max(steps, int(d))
+            if steps == 9:
+                return steps
+        
+        return steps
 
 def main():
     s = Solution()
-    print(s.solve())
+    print(s.solve("32"))
+    print(s.solve("82734"))
+    print(s.solve("27346209830709182346"))
 
 if __name__ == '__main__':
     main()

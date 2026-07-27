d10cc7460690514d2ebc5b0c0f150a71eace430c
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-07-27 18:34:55 -0400

    iniit

---
+++ 2639_find_column_width.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/find-the-width-of-columns-of-a-grid/
+class Solution:
+    def findColumnWidth(self, grid: list[list[int]]) -> list[int]:
+        
+
+
+def main():
+    s = Solution()
+    print(s.findColumnWidth([[1],[22],[333]]))
+
+if __name__ == '__main__':
+    main()

784f38c8c46af086af93b8a96dce2d7e70628fe7
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-07-27 20:48:15 -0400

    init

---
+++ 1508_range_sum.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/range-sum-of-sorted-subarray-sums/description/
+class Solution:
+    def rangeSum(self, nums: list[int], n: int, left: int, right: int) -> int:
+        
+
+
+def main():
+    s = Solution()
+    print(s.rangeSum([1,2,3,4],4,1,5))
+
+if __name__ == '__main__':
+    main()

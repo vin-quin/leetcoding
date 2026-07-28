8590a0e68003c19047315f6ea129c4938756cc86
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-07-27 20:36:27 -0400

    init

---
+++ 503_next_greater_elements.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/next-greater-element-ii/
+class Solution:
+    def solve(self, nums: list[int]) -> list[int]:
+        ...
+
+
+def main():
+    s = Solution()
+    print(s.solve([1,2,1]))
+
+if __name__ == '__main__':
+    main()

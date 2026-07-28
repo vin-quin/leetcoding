65ca07a83781c1fc07df16cef0b9717532930b32
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-07-27 20:42:30 -0400

    done

--- 503_next_greater_elements.py
+++ 503_next_greater_elements.py
@@ -1,12 +1,22 @@
 # https://leetcode.com/problems/next-greater-element-ii/
 class Solution:
     def solve(self, nums: list[int]) -> list[int]:
-        ...
+        nexts = []
 
+        for i in range(len(nums)):
+            for j in range(i, i+len(nums)):
+                if nums[j%len(nums)] > nums[i]:
+                    nexts.append(nums[j%len(nums)])
+                    break
+            else: # If this loop finishes we never found a next greater
+                nexts.append(-1)
+
+        return nexts
 
 def main():
     s = Solution()
     print(s.solve([1,2,1]))
+    print(s.solve([1,2,3,4,3]))
 
 if __name__ == '__main__':
     main()

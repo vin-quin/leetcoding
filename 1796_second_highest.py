e82e2ce1d90dfeab7d696773f50973af12a8b18d
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-07-28 04:34:04 -0400

    done

---
+++ 1796_second_highest.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/second-largest-digit-in-a-string/
+class Solution:
+    def solve(self, s: str) -> int:
+        nums = [-1, -1]
+
+        for c in s:
+            if not c.isdigit():
+                continue
+
+            if int(c) > nums[0]:
+                nums[0], nums[1] = int(c), nums[0]
+            elif int(c) > nums[1] and int(c) != nums[0]:
+                nums[1] = int(c)
+        
+        return nums[1]
+
+def main():
+    s = Solution()
+    print(s.solve("dfa12321afd"))
+    print(s.solve("abc1111"))
+
+if __name__ == '__main__':
+    main()

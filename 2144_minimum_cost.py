7a8db34de61208a91112d4ab8235c382c2c4af87
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-07-28 21:28:56 -0400

    init

---
+++ 2144_minimum_cost.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/minimum-cost-of-buying-candies-with-discount/   
+class Solution:
+    def solve(self, cost: list[int]) -> int:
+        ...
+
+
+def main():
+    s = Solution()
+    print(s.solve([1,2,3]))
+
+if __name__ == '__main__':
+    main()

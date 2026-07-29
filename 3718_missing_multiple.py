069a171738376fffdaf46f7d0f01703c566ff50e
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-07-28 21:27:29 -0400

    done

--- 3718_missing_multiple.py
+++ 3718_missing_multiple.py
@@ -1,12 +1,19 @@
 # https://leetcode.com/problems/smallest-missing-multiple-of-k/description/
 class Solution:
     def solve(self, nums: list[int], k :int) -> int:
-        ...
+        nums.sort()
+        target = k
+
+        for n in nums:
+            if n == target:
+                target += k
+        
+        return target
 
 
 def main():
     s = Solution()
-    print(s.solve([8,2,3,4,6]))
+    print(s.solve([8,2,3,4,6], 2))
 
 if __name__ == '__main__':
     main()

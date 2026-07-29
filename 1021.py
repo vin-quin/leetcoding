ff771a35e6f5f27da8887e9ac3671c625c1ceb76
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-07-28 21:38:11 -0400

    init

---
+++ 1021.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/remove-outermost-parentheses/   
+class Solution:
+    def solve(self, s: str) ->  str:
+        ...
+
+
+def main():
+    s = Solution()
+    print(s.solve("(()())(())"))
+
+if __name__ == '__main__':
+    main()

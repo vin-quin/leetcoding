ce496df63f33515ce22e4ce6c0df0cb77b3e68d7
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-07-28 21:51:27 -0400

    done

--- 1021.py
+++ 1021.py
@@ -1,12 +1,25 @@
 # https://leetcode.com/problems/remove-outermost-parentheses/   
 class Solution:
     def solve(self, s: str) ->  str:
-        ...
+        stack = []
+        ss = ""
 
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                j = stack.pop()
+
+                if len(stack) == 0: # This was an outermost paren
+                    ss += s[j+1:i]
+
+        return ss
 
 def main():
     s = Solution()
     print(s.solve("(()())(())"))
+    print(s.solve("(()())(())(()(()))"))
+    print(s.solve("()()"))
 
 if __name__ == '__main__':
     main()

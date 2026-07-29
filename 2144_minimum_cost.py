b5b21cc0329e96af32540a5ac9c8f7d9ab4501f2
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-07-28 21:35:57 -0400

    done

--- 2144_minimum_cost.py
+++ 2144_minimum_cost.py
@@ -3,16 +3,17 @@ class Solution:
     def solve(self, cost: list[int]) -> int:
         cost.sort()
 
-        for i in range(len(cost)-3, -1, -2): # Every third candy iis free so set to 0
+        for i in range(len(cost)-3, -1, -3): # Every third candy iis free so set to 0
             cost[i] = 0
 
         return sum(cost)
 
 def main():
     s = Solution()
-    print(s.solve([1,2,3]))
-    print(s.solve([6,5,7,9,2,2]))
-    print(s.solve([5,5]))
+    # print(s.solve([1,2,3]))
+    # print(s.solve([6,5,7,9,2,2]))
+    # print(s.solve([5,5]))
+    print(s.solve([10,5,9,4,1,9,10,2,10,8]))
 
 if __name__ == '__main__':
     main()

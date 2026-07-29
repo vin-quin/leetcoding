52cba467cc0e69efaaefd821b3e87b1e069789ac
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-07-28 21:55:24 -0400

    init

---
+++ 485.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/max-consecutive-ones/
+class Solution:
+    def solve(self, nums: list[int]) -> int:
+        score, cnt = 0, 0
+
+        for n in nums:
+            if n == 1:
+                count += 1
+            else: 
+                count = 0
+            
+            score = max(score, count)
+
+        return score
+
+def main():
+    s = Solution()
+    print(s.solve([1,1,0,1,1,1]))
+
+if __name__ == '__main__':
+    main()

a0c6a4cd4484f62d215b65738618e68cd7546675
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-07-28 21:33:30 -0400

    done

--- 2144_minimum_cost.py
+++ 2144_minimum_cost.py
@@ -1,12 +1,18 @@
 # https://leetcode.com/problems/minimum-cost-of-buying-candies-with-discount/   
 class Solution:
     def solve(self, cost: list[int]) -> int:
-        ...
+        cost.sort()
 
+        for i in range(len(cost)-3, -1, -2): # Every third candy iis free so set to 0
+            cost[i] = 0
+
+        return sum(cost)
 
 def main():
     s = Solution()
     print(s.solve([1,2,3]))
+    print(s.solve([6,5,7,9,2,2]))
+    print(s.solve([5,5]))
 
 if __name__ == '__main__':
     main()

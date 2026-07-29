e59073cac2ebf9ccc5210643187bd1c51bc85d4d
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-07-28 21:57:30 -0400

    init

---
+++ 796.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/rotate-string/
+class Solution:
+    def solve(self, s: str, goal: str) -> bool:
+        
+
+def main():
+    s = Solution()
+    print(s.solve("abcde", "cdeab"))
+
+if __name__ == '__main__':
+    main()

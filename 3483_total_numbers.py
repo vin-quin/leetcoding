63e9ce5ee994bd77586608149081a7ec8997d6e9
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-07-28 20:01:11 -0400

    init

---
+++ 3483_total_numbers.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/unique-3-digit-even-numbers/
+class Solution:
+    def solve(self, digits: list[int]) -> int:
+        ...
+
+
+def main():
+    s = Solution()
+    print(s.solve([1,2,3,4]))
+
+if __name__ == '__main__':
+    main()

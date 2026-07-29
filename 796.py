1c91cd2e339a0e06a2e9601f2904ec788aee979a
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-07-28 22:00:53 -0400

    done

--- 796.py
+++ 796.py
@@ -1,11 +1,21 @@
 # https://leetcode.com/problems/rotate-string/
 class Solution:
     def solve(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
         
+        attempts = 0
+
+        while s != goal and attempts != len(s):
+            s = s[1:] + s[0]
+            attempts += 1
+
+        return s == goal
 
 def main():
     s = Solution()
     print(s.solve("abcde", "cdeab"))
+    print(s.solve("abcde", "abced"))
 
 if __name__ == '__main__':
     main()

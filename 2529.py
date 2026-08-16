e93528f784d3adfc01266fb1fd67e26a66e57f32
Vincent <44277279+vin-quin@users.noreply.github.com>
2026-08-16 19:15:28 -0400

    iiniit

---
+++ 2529.py
@@ -0,0 +1,13 @@
+# 
+class Solution:
+    def solve(self, nums: list[int]) -> int:
+        ...
+
+
+def main():
+    s = Solution()
+    # print(s.solve([-2,-1,-1,1,2,3]))
+    print(s.solve([-3,-2,-1,0,0,1,2]))
+
+if __name__ == '__main__':
+    main()
